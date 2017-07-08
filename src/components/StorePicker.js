632b765357272b7131201e0821cbf174d146a772
Maja Kudlicka <maja.kudlicka@gmail.com>
2017-07-08 16:46:51 +0100

    added react router and getFunname function

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
-     return (
-       <form className="store-selector">
-       <input type="text" required placeholder="Store Name" />
-       <button type="submit">Visit store-></button>
-       </form>
-     )
+    // Any where else
+    return (
+      <form className="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <button type="submit">Visit Store →</button>
+      </form>
+    )
   }
 }
 
